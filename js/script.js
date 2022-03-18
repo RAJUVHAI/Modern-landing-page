46b5d634d01ea86cf68144f2eab33e3235b6f7d8
Imranraj323 <imranmahamudraja535600@gmail.com>
2022-03-18 19:50:40 +0600

    Preloader added

--- js/script.js
+++ js/script.js
@@ -45,3 +45,19 @@ filters.forEach(filter => {
 
     });
 });
+
+
+// custom cursor
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
